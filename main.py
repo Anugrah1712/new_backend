0ab09470878e9558812c1a5d95204b7e1c6bb113
Anugrah <apple@APPLEs-MacBook-Pro.local>
2025-03-28 12:03:49 +0530

    Updated CORS

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000"
-        "*"  # ⚠️ Avoid using this in production
+        "*"  
     ],
     allow_credentials=True,
     allow_methods=["*"],
