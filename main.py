4d207931a6d39adc3f907f48f2e981ea95706d6b
Anugrah <apple@APPLEs-MacBook-Pro.local>
2025-03-28 16:42:07 +0530

    Updated CORS

--- main.py
+++ main.py
@@ -26,10 +26,7 @@ app = FastAPI()
 # Allow frontend to access backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000"
-        "*"  
-    ],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
