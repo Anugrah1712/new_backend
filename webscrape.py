3ae34fd0a2f98079f9798b401624c186393e220f
Anugrah <anugrah@Anugrahs-MacBook-Air.local>
2025-05-07 15:41:05 +0530

    updates

--- webscrape.py
+++ webscrape.py
@@ -58,7 +58,7 @@ async def scrape_web_data(links):
 
     async with async_playwright() as p:
         browser = await p.chromium.launch(
-            headless=False,
+            headless=True,
             args=[
                 "--no-sandbox",
                 "--disable-setuid-sandbox",
